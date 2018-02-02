43d45a0c7b564314d22f10954625da3eb8e00be6
naomidesign <naomipastor.design@gmail.com>
2018-02-02 21:47:26 +0100

    he hecho el ejercicio uno de nuevo

---
+++ nao/repaso01.js
@@ -0,0 +1,28 @@
+// rehacer ejercicios clase 2
+
+//ejercicio 1
+
+/*
+1 - Diseña un programa que lea dos números y realice los siguientes cálculos:
+
+Valor de su suma
+Valor de su resta
+Valor de su division
+Valor de su producto
+*/
+
+var num = Number(prompt("Introduce un número"));
+var num2 = Number(prompt("Introduce otro número"));
+
+function resultadoSuma () {
+	resultadoSuma = num + num2;
+	if (isNaN(resultadoSuma)) {
+		console.log("error, eso no es un número")
+	} else {
+		return resultadoSuma
+	}
+}
+
+resultadoSuma();
+
+
